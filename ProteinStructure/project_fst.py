699d889a18c95e49439e563bbbaafaae9bac5f11
Ryan Nguyen <rnwin7@gmail.com>
2017-11-15 12:42:41 -0800

    ISHIFT WAS THE PROBLEM ALL ALONG

--- ProteinStructure/project_fst.py
+++ ProteinStructure/project_fst.py
@@ -17,21 +17,17 @@ def project_fst(mol, R):
 	N = len(mol)
 	rho_hat = np.fft.fftn(mol)
 	rho_hat = np.fft.fftshift(rho_hat)
-	wx, wy, wz = np.meshgrid(np.arange(-(N-1)/2, (N-1)/2 +1),np.arange(-(N-1)/2, (N-1)/2 +1),np.arange(-(N-1)/2, (N-1)/2 +1))
-	rho_hat=(np.exp(np.pi*1j*(wx+wy+wz))/(N**3))*(rho_hat)
+	wx, wy, wz = np.meshgrid(np.arange(-(N-1)/2, (N-1)/2 +1, dtype= int),np.arange(-(N-1)/2, (N-1)/2 +1,dtype= int),np.arange(-(N-1)/2, (N-1)/2 +1,dtype= int))
+	rho_hat=((-1)**np.abs(wx+wy+wz)/(N**3)*rho_hat) #np.exp(np.pi*1j*(wx+wy+wz))/(N**3))*(rho_hat)
 	
 	print("Now running Linear Interpolation")
 	N_range = np.arange(-(N-1)/2, (N-1)/2 +1)
 	rho_hat = RGI((N_range,N_range,N_range), rho_hat, bounds_error= False, fill_value=0)
 
 	### make 2D mesh that takes slice of rho hat which NxNx3 grid 
-	### to do this, we must find the etas
 	print("Creating 2D Sampling grid")
-	eta_x, eta_y= np.meshgrid(np.arange(-(N-1)/2, (N-1)/2 +1),np.arange(-(N-1)/2, (N-1)/2 +1), indexing= "ij")
-	for i in range(N):
-		for k in range(N):
-			eta_x[i,k]= -N/2 + 1 + k
-			eta_y[i,k]= -N/2 + 1 + i
+	eta_x, eta_y= np.meshgrid(np.arange(-(N-1)/2, (N-1)/2 +1, dtype= int),np.arange(-(N-1)/2, (N-1)/2 +1, dtype= int), indexing= "ij")
+
 	eta_x= eta_x[...,np.newaxis]
 	eta_y= eta_y[...,np.newaxis]
 	
@@ -43,11 +39,11 @@ def project_fst(mol, R):
 
 	sample_grid = rho_hat(grid)
 	
-	em_slice = (np.exp(-np.pi*1j*(eta_x[...,0]+eta_y[...,0]))*(N**2))*sample_grid
+	em_slice=(-1)**np.abs(eta_x[...,0]+eta_y[...,0])*(N**2)*sample_grid
 
-	em_slice = np.fft.fftshift(em_slice)
+	em_slice = np.fft.ifftshift(em_slice)
 	em_slice = np.fft.ifftn(em_slice)
-	print(np.imag(em_slice))
+	#print(np.linalg.norm(np.imag(em_slice)))
 
 	return np.real(em_slice)
 
