f78ea9b5b8c92fc9af0c64c89039b0a465f3f041
Ryan Nguyen <rnwin7@gmail.com>
2017-12-02 11:55:24 -0800

    back project works babygit add *git add *

--- ProteinStructure/reconstruct.py
+++ ProteinStructure/reconstruct.py
@@ -11,8 +11,7 @@ import sys
 def reconstruct(image_array, R_array): 
 	N = image_array[0].shape[0] 
 	B = np.zeros((N,N,N)) 
- 	
-	#print(R_array[0])
+	L = np.zeros((N,N,N))
 
 	for i in range(0, len(image_array)):
 		
@@ -21,40 +20,46 @@ def reconstruct(image_array, R_array):
 		image_hat = np.fft.fftshift(image_hat)
 		freq_range = np.arange(-(N-1)/2, (N-1)/2 +1, dtype= int) 
 		wx_j, wy_j = np.meshgrid(freq_range, freq_range)
-		image_hat = ((-1)**np.abs(wx_j+wy_j)/(N**2)*image_hat)
+		image_hat = (((-1)**np.abs(wx_j+wy_j))/N**2)*image_hat
 		image_hat = np.tile(image_hat[...,np.newaxis], [1,1,N])
 
 		print("Computing l_j_hat")
-		#wlx_j, wly_j, wlz_j = np.meshgrid(freq_range, freq_range, freq_range)
-		wlz_j = freq_range#np.meshgrid(freq_range)
-		l_j = N * np.sinc(N * np.pi * wlz_j)
+		wz_j = freq_range
+		l_j = N * np.sinc(N * np.pi * wz_j)
 		l_j_hat = np.tile(l_j[np.newaxis,np.newaxis,...],[N,N,1])
-		#l_j_hat = ((-1)**np.abs(wlx_j+wly_j+wlz_j)/(N**3)*l_j_hat)
-		
+
+
 		print("Creating rotated grid")
 
-		#np.array((freq_range,freq_range,3))
-		rot_grid = np.meshgrid(freq_range, freq_range, freq_range)
-		#d = np.zeros((freq_range, freq_range, freq_range, 3))
-		rot_grid = np.tensordot(rot_grid, R_array[i], axes = )
-		print(type(rot_grid))
+		wlx, wly, wlz = np.meshgrid(freq_range, freq_range, freq_range)
+		rot_grid = np.zeros((N,N,N,3))
 
-		sys.exit()
-		print("Computing b_j_hat")
+		for j in range(N):
+			for k in range(N):
+				for l in range(N):
+					point = np.array([wlx[j,k,l], wly[j,k,l], wlz[j,k,l]]) 
+					rot_grid[j,k,l] = np.dot(R_array[i], point) 
 		
-		b_j_hat = image_hat * l_j_hat
-		b_j_hat_fnc = RGI((freq_range,freq_range,freq_range), b_j_hat, bounds_error= False, fill_value=0)
-		b_j_hat = b_j_hat_fnc(rot_grid)
+		image_hat_fnc = RGI((freq_range,freq_range,freq_range), image_hat, bounds_error= False, fill_value=0)
+		b_j_hat_fnc = RGI((freq_range,freq_range,freq_range), l_j_hat, bounds_error= False, fill_value=0)
 
+		image_hat = image_hat_fnc(rot_grid)
+		l_j_hat = image_hat_fnc(rot_grid)
+
+
+
+
+		b_j_hat = image_hat * l_j_hat
+		b_j_hat = ((-1)**np.abs(wlx+wly+wlz)*(N**3))*b_j_hat
 		b_j_hat = np.fft.ifftshift(b_j_hat)
 		b_j = np.fft.ifftn(b_j_hat)
 
-		print(np.real(b_j))
-		#b_j = ((-1)**np.abs(wx_j+wy_j+wz_j)/(N**3)*b_j_hat)
+		L += np.real(l_j_hat) 
 		B += np.real(b_j)
+
 	return np.float32(np.real(B))
 
-num_images = 1
+num_images = 10
 zika_file = mrcfile.open('zika_153.mrc')
 rho = zika_file.data
 
@@ -68,6 +73,8 @@ for _ in range(0,num_images):
 	R_array.append(R)
 print("Now getting image")
 back_img = reconstruct(image_array, R_array)
+#back_img[np.isnan(back_img)]=0
+
 print("Now saving")
 with mrcfile.new('full2.mrc', overwrite=True) as mrc:
 	mrc.set_data(back_img)
